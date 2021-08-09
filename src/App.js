c7d8ac787bf3aa70d14bf17f0c60a41d0f7f50be
james <jameshoggard.dev@gmail.com>
2021-08-09 19:53:42 -0400

    updated1950

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleLand from "./pages/SingleLand";
 import Error from "./pages/Error";
-// import components
 import Navbar from "./components/Navbar";
 
 export default function App() {
-
-  // const [lists, setLists] = useState([]);
-  
-  // handleClick = () = {
-  //  
-  //   fetch("http://127.0.0.1:6001/lists", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       id: "",
-  //       user_id: "1",
-  //       land_id: "",
-  //     }),
-  //   })
-  //     .then((r) => r.json())
-  //     .then((console.log));
-  //     // setLists(lists)
-  // }
-
   const handleClick = (event, landId) => {
-    
-    // debugger;
     fetch("http://127.0.0.1:6001/lists", {
       method: "POST",
       headers: {
@@ -47,7 +21,6 @@ export default function App() {
     })
     .then((response) => response.json())
     .then((response) => {
-      // console.log(response);
     })
     .catch((error) => console.log(error));
   };
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,51 +1,45 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import Land from '../components/Land.js'
-import Loading from '../components/Loading.js'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Land from "../components/Land.js";
+import Loading from "../components/Loading.js";
+import { useGlobalContext } from "../context";
 
 export default function About() {
   const [favorites, setFavorites] = useState([]);
-  // const [newLands, setnewLands] = useState([]);
-
-  const { lands, loading } = useGlobalContext()
-  // console.log(lands)   //==filteredLands
+  const { lands, loading } = useGlobalContext();
 
   useEffect(() => {
     fetch("http://127.0.0.1:6001/lists")
-      .then(res => res.json())
-      .then(listsArr => {
-      setFavorites(listsArr);
-      console.log(listsArr);
-  });
-  }, [])
+      .then((res) => res.json())
+      .then((listsArr) => {
+        setFavorites(listsArr);
+        console.log(listsArr);
+      });
+  }, []);
 
-  if(favorites){
-    lands.filter((land) => land.list_id === 1)
+  if (favorites) {
+    lands.filter((land) => land.list_id === 1);
   }
 
-
   if (loading) {
-    return <Loading/>
+    return <Loading />;
   }
   if (lands.length < 1) {
     return (
-      <h2 className='section-title'>
+      <h2 className="section-title">
         no countries matched your search criteria
       </h2>
-    )
+    );
   }
-
-
   return (
-    <section className='section'>
-      <h2 className='section-title'>Countries</h2>
-      <div className='cocktails-center'>
+    <section className="section">
+      <h2 className="section-title">Countries</h2>
+      <div className="cocktails-center">
         {lands.map((land) => {
-          return <Land key={land.id} {...land} />
+          return <Land key={land.id} {...land} />;
         })}
       </div>
     </section>
-  )
+  );
 }
