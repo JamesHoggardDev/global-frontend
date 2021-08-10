e28ee45001b738f26952b64d0908f4beacda8d3c
james <jameshoggard.dev@gmail.com>
2021-08-09 20:03:01 -0400

    updated18

--- src/context.js
+++ src/context.js
@@ -1,31 +1,24 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'http://127.0.0.1:6001/lands'
-const AppContext = React.createContext()
+const url = "http://127.0.0.1:6001/lands";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true)
-  const [searchTerm, setSearchTerm] = useState('')
-  const [lands, setLands] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [lands, setLands] = useState([]);
 
-  const fetchLands = useCallback( async () => {
-    setLoading(true)
+  const fetchLands = useCallback(async () => {
+    setLoading(true);
     try {
-      const response = await fetch(`${url}`)
-      
-      const landsArr = await response.json()
+      const response = await fetch(`${url}`);
+
+      const landsArr = await response.json();
 
       if (landsArr) {
         const lands = landsArr.map((land) => {
-          const {
-            id,
-            name,
-            flag,
-            capital,
-            currency,
-            list_id,
-          } = land
+          const { id, name, flag, capital, currency, list_id } = land;
 
           return {
             id: id,
@@ -33,39 +26,34 @@ const AppProvider = ({ children }) => {
             image: flag,
             capital: capital,
             currency: currency,
-            list_id: list_id
-          }
-        })
+            list_id: list_id,
+          };
+        });
 
         const filteredLands = lands.filter((land) => {
           return land.name.toLowerCase().includes(searchTerm.toLowerCase());
         });
-        // console.log(filteredLands)
-        setLands(filteredLands)
+        setLands(filteredLands);
       } else {
-        setLands([])
+        setLands([]);
       }
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  },[searchTerm])
+  }, [searchTerm]);
   useEffect(() => {
-    fetchLands()
-  }, [searchTerm, fetchLands])
+    fetchLands();
+  }, [searchTerm, fetchLands]);
 
   return (
-    <AppContext.Provider
-      value={{ loading, lands, searchTerm, setSearchTerm }}
-    >
+    <AppContext.Provider value={{ loading, lands, searchTerm, setSearchTerm }}>
       {children}
     </AppContext.Provider>
-  )
-}
-
+  );
+};
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
-
-export { AppContext, AppProvider }
+  return useContext(AppContext);
+};
+export { AppContext, AppProvider };
--- src/pages/SingleLand.js
+++ src/pages/SingleLand.js
@@ -1,25 +1,20 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
+import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
 
 export default function SingleLand({ handleClick }) {
-  const { id } = useParams()
-  const landId = id
-  // console.log( landId )
-  
-  const [loading, setLoading] = React.useState(false)
-  const [land, setLand] = React.useState(null)
+  const { id } = useParams();
+  const landId = id;
+
+  const [loading, setLoading] = React.useState(false);
+  const [land, setLand] = React.useState(null);
 
   React.useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     async function getLand() {
       try {
-        const response = await fetch(
-          `http://127.0.0.1:6001/lands/${id}`
-        )
-        const data = await response.json()
-        // console.log(data)   
-        // debugger;
+        const response = await fetch(`http://127.0.0.1:6001/lands/${id}`);
+        const data = await response.json();
         if (data) {
           const {
             id: id,
@@ -27,83 +22,59 @@ export default function SingleLand({ handleClick }) {
             flag: image,
             capital: capital,
             currency: currency,
-          } = data
-          
+          } = data;
+
           const newLand = {
             id,
             name,
             image,
             capital,
             currency,
-          }
-          setLand(newLand)
+          };
+          setLand(newLand);
         } else {
-          setLand(null)
+          setLand(null);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      setLoading(false)
+      setLoading(false);
     }
-    getLand()
-  }, [id])
+    getLand();
+  }, [id]);
   if (loading) {
-    return <Loading/>
+    return <Loading />;
   }
   if (!land) {
-    return <h2 className='section-title'>no land to display</h2>
+    return <h2 className="section-title">no land to display</h2>;
   } else {
-    const {
-      // id,
-      name,
-      image,
-      capital,
-      currency,
-    } = land
+    const { name, image, capital, currency } = land;
     return (
-      <section className='section cocktail-section'>
-        <Link to='/' className='btn btn-primary'>
+      <section className="section cocktail-section">
+        <Link to="/" className="btn btn-primary">
           back home
         </Link>
-        
-        <h2 className='section-title'>{name}</h2>
-        <div key={id} onClick={(e) => handleClick(e, landId)} className='drink'>
+
+        <h2 className="section-title">{name}</h2>
+        <div key={id} onClick={(e) => handleClick(e, landId)} className="drink">
           <img key={id} src={image} alt={name}></img>
-          <div className='drink-info'>
-          {/* <Link to='/' className='btn btn-primary'>
-          Favorite
-        </Link> */}
-        
-         <button  onClick={console.log()} className='btn btn-primary'>Favorite</button>  
-        
-        <br>
-        </br>
-        <br>
-        </br>
+          <div className="drink-info">
+            <button onClick={console.log()} className="btn btn-primary">
+              Favorite
+            </button>
+
+            <br></br>
+            <br></br>
             <p>
-              <span className='drink-data'>name :</span> {name}
-            </p>
-            {/* <p>
-              <span className='drink-data'>category :</span> {category}
-            </p> */}
-            <p>
-              <span className='drink-data'>capital :</span> {capital}
+              <span className="drink-data">name :</span> {name}
+              <span className="drink-data">capital :</span> {capital}
             </p>
             <p>
-              <span className='drink-data'>currency :</span> {currency}
+              <span className="drink-data">currency :</span> {currency}
             </p>
-            {/* <p>
-              <span className='drink-data'>instructons :</span> {instructions}
-            </p> */}
-            {/* <p>
-              <span className='drink-data'>ingredients :</span>
-              {ingredients.map((item, index) => {
-                return item ? <span key={index}> {item}</span> : null
-              })}
-            </p> */}
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
