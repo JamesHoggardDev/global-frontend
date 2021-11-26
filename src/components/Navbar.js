e0d6c18afb2e57a480e5faac94ef0d0f92bc3972
James <Jameshoggard.dev@gmail.com>
2021-11-25 22:53:03 -0500

    upd20

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <div className='nav-center'>
-        <Link to='/'>
+        {/* <Link to='/'>
           <img src={logo} alt='cocktail db logo' className='logo' />
-        </Link>
+        </Link> */}
         <ul className='nav-links'>
           <li>
             <Link to='/'>home</Link>
